8c6f47c9cb4239185b6a00dbf64e6cb76ab061f2
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:31:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+
+ String nome = "erick";
+   int dia = 25;
+   int mes = 7;
+   int ano = 2009;
+   double pi = 3.14159;
+  
+   System.out.printf("o valor  de pi é: %2f );              
+  }
+
+  
+}
